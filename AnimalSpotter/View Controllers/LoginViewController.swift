5ba14ada4df0b4a5508de4b2a2cba39a8c7b6ab1
John Kouris <jdkouris@gmail.com>
2019-10-04 10:45:12 -0700

    Added the outlets, actions, and button design to the loginViewController

--- AnimalSpotter/View Controllers/LoginViewController.swift
+++ AnimalSpotter/View Controllers/LoginViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+        signInButton.tintColor = .white
+        signInButton.layer.cornerRadius = 8.0
     }
     
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+    }
     /*
     // MARK: - Navigation
 
