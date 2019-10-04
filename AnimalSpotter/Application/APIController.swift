3c78f2938d582cb2053884d64dbba39f912f9728
John Kouris <jdkouris@gmail.com>
2019-10-04 11:41:12 -0700

    Filled out the signUp method

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -21,8 +21,49 @@ class APIController {
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
     
     // create function for sign up
-//    func signUp(with user: User)
+    func signUp(with user: User, completion: @escaping (Error?) -> Void) {
+        // Build the URL
+        let requestURL = baseUrl
+            .appendingPathComponent("users")
+            .appendingPathComponent("signup")
+        
+        // Build the request
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        // Tell the API that the body is in JSON format
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            let userJSON = try encoder.encode(user)
+            request.httpBody = userJSON
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+        }
+        
+        // Perform the request (data task)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Handle errors
+            if let error = error {
+                NSLog("Error signing up user: \(error)")
+                completion(error)
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                let statusCodeError = NSError(domain: "com.JohnKouris.AnimalSpotter", code: response.statusCode, userInfo: nil)
+                completion(statusCodeError)
+            }
+            
+            // nil means there was no error, everything succeeded
+            completion(nil)
+        }.resume()
+    }
     
     // create function for sign in
+    
     
 }
