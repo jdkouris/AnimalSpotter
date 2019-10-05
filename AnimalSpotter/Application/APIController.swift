30f656b70cf19dbf6e60e779bef133c21ca81daf
John Kouris <jdkouris@gmail.com>
2019-10-05 09:52:14 -0700

    Created the signIn method in the APIController

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -19,6 +19,7 @@ enum HTTPMethod: String {
 class APIController {
     
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
+    var bearer: Bearer?
     
     // create function for sign up
     func signUp(with user: User, completion: @escaping (Error?) -> Void) {
@@ -64,6 +65,52 @@ class APIController {
     }
     
     // create function for sign in
-    
+    func signIn(with user: User, completion: @escaping (Error?) -> Void) {
+        let requestURL = baseUrl
+            .appendingPathComponent("users")
+            .appendingPathComponent("login")
+        
+        var request = URLRequest(url: requestURL)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(user)
+        } catch {
+            NSLog("Error encoding user for sign in: \(error)")
+            completion(error)
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                NSLog("Error signing in user: \(error)")
+                completion(error)
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                let statusCodeError = NSError(domain: "com.JohnKouris.AnimalSpotter", code: response.statusCode, userInfo: nil)
+                completion(statusCodeError)
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                let noDataError = NSError(domain: "com.JohnKouris.AnimalSpotter", code: -1, userInfo: nil)
+                completion(noDataError)
+                return
+            }
+            
+            do {
+                let bearer = try JSONDecoder().decode(Bearer.self, from: data)
+                self.bearer = bearer
+            } catch {
+                NSLog("Error decoding the bearer token: \(error)")
+                completion(error)
+            }
+            
+            completion(nil)
+        }.resume()
+    }
     
 }
