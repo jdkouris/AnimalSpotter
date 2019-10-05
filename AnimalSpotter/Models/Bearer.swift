d5b1ebfc583da0e0ae21289e4cfa8a554ccdcf95
John Kouris <jdkouris@gmail.com>
2019-10-05 09:48:10 -0700

    Created the Bearer struct

---
+++ AnimalSpotter/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by John Kouris on 10/5/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
