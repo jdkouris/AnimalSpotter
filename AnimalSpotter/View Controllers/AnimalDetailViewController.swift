1ad504f6126c3011ee82cb0c222ebd61bdf959e6
John Kouris <jdkouris@gmail.com>
2019-10-07 10:33:57 -0700

    Added the AnimalDetailViewController file and created the outlets

---
+++ AnimalSpotter/View Controllers/AnimalDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AnimalDetailViewController.swift
+//  AnimalSpotter
+//
+//  Created by John Kouris on 10/7/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class AnimalDetailViewController: UIViewController {
+
+    @IBOutlet var timeSeenLabel: UILabel!
+    @IBOutlet var corrdinatesLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var animalImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
